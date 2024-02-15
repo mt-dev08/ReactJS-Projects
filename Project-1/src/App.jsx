585f3b0a073b93536c57bd81670c9609cd8e7b21
mt-dev08 <miantalha.t08@gmail.com>
2024-02-16 01:11:02 +0500

    ReactJS Pro 1 Files

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navigation from './components/Navigation.jsx';
+import Hero from './components/Hero.jsx';
+const App =()=>{
+
+    return(
+    <>
+   <Navigation/>
+   <Hero/>
+    </>
+    )
+}
+
+export default App;
